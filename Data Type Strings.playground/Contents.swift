0588a8fbf0adea3a8f35d928f8368257d6b7bc80
Joanna Tzu-Hsuan Huang <joannathhuang@Joannas-MacBook-Pro.local>
2016-08-30 09:53:22 -0400

    Done

---
+++ Data Type Strings.playground/Contents.swift
@@ -0,0 +1,14 @@
+let thirdPlanet = "Earth"
+
+var ninthPLanet: String
+var ninthPlanet = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+let sixthPlanet: String = "Saturn"
+let seventhPlanet = "Uranus"
+
+let numberOfCountriesInAfrica = 54
+
+let sixPlanet = "Saturn"
+let numberOfMoonsOfSaturn = 62
+print ("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixPlanet)")
